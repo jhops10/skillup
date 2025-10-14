081e1ccdb30eb5694dd65128ebca4580264f26c8
Joao Paulo <joaowais@gmail.com>
2025-10-14 17:44:06 -0300

    first commit

---
+++ src/main/java/com/jhops10/skillup/SkillupApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.skillup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/skillup/SkillupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.skillup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
